ba975df97cf9d4df6ddf8aad65a075e0b35b3618
btarunkumar <btarun@gmail.com>
2024-08-25 08:37:16 +0530

    Adding dist file

---
+++ dist/browser/seoData/seoData.service.ts
@@ -0,0 +1,39 @@
+export const seoData = {
+    signin:   {      
+        "title": "yvs - Sign In", 
+        "description": "Sign In yvs is an OTT platform, where you can watch action, romantic, thriller, and comedy movies etc. Watch yvs anywhere anytime. Download the app now.", 
+        "keywords": "Sign In , Login , Sign in yvs , Login to yvs", 
+        "metaTags": [
+            { 
+                "tagType": "og:title", 
+                "tagName": "property", 
+                "content": "yvs - Sign In" }, 
+           
+        ]
+    },
+    signup:   {      
+        "title": "yvs - Sign Up", 
+        "description": "Register in yvs is an OTT platform, where you can watch action, romantic, thriller, and comedy movies etc. Watch yvs anywhere anytime. Download the app now.", 
+        "keywords": "Sign up , Join, register , Create Account", 
+        "metaTags": [
+            { 
+                "tagType": "og:title", 
+                "tagName": "property", 
+                "content": "yvs - Sign Up" }, 
+           
+        ]
+    },
+
+    support :  {      
+        "title": "yvs - Help / Support", 
+        "description": "Reach us Any time , check terms , privacy and cookie policy", 
+        "keywords": "Help Center , Terms . privacy , cokkie policy , Reach Us , contact us ,Faq", 
+        "metaTags": [
+            { 
+                "tagType": "og:title", 
+                "tagName": "property", 
+                "content": "yvs - Help / Support" }, 
+            
+        ]
+    },
+}
